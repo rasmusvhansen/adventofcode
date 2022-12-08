e2d2ec806d915c151f3fdcf8386ac5527e46a79c
rasmusvhansen <rasmusvhansen@gmail.com>
2022-12-08 15:50:23 +0100

    added to matrix op

--- src/util.ts
+++ src/util.ts
@@ -3,3 +3,17 @@ export const times = (a: number, b: number) => a * b;
 export const sum = (ns: number[]) => ns.reduce(add, 0);
 export const multiply = (ns: number[]) => ns.reduce(times, 1);
 export const range = (start: number, end: number) => Array.from({ length: end - start + 1 }).map((_, i) => i + start);
+export const toMatrix = <T = number>(input: string, map: (s: string, row: number, col: number) => T = (s) => +s as T) => {
+  const matrix = new Map<string, T>();
+  input.split('\n').map((s, row) => s.split('').map((n, col) => matrix.set(matrixKey(row, col), map(n, row, col))));
+  return {
+    get: (row: number, col: number) => matrix.get(matrixKey(row, col)),
+    has: (row: number, col: number) => matrix.has(matrixKey(row, col)),
+    forEach: matrix.forEach.bind(matrix) as Map<string, T>['forEach'],
+    get size() {
+      return matrix.size;
+    },
+  };
+};
+
+const matrixKey = (row: number, col: number) => `${row},${col}`;
